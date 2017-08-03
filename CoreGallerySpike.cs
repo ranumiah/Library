e00085a026ffcc39ed7282bb82aa220b1a38c9bb
Ranu <O@O.net>
2017-08-03 16:57:30 +0100

    Add CoreGallerySpike.cs

---
+++ CoreGallerySpike.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreGallery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult Gallery()
+        {
+            var foo = new Foo();
+            foo.Images = Directory.GetFiles(@"C:\img\");
+            return View(foo);
+        }
+    }
+
+    public class Foo
+    {
+        public string[] Images { get; set; }
+    }
+}
